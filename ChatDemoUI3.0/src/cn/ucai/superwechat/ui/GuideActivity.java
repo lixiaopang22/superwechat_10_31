278ac90419c45bae398a4679ada3c4faf15ae357
lixiaopang22 <1021733560@qq.com>
2016-11-01 20:48:06 +0800

    添加butter knife，导入工具类，实现跳转登录，注册页面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,12 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
@@ -10,5 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.bt_login, R.id.bt_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_login:
+                MFGT.gotoLogin(this);
+                break;
+            case R.id.bt_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
     }
 }
