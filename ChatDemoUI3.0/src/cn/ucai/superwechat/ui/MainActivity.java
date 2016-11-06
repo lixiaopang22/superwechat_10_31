7f63128b94e8faa119db9b58c10277f7082b2f5d
lixiaopang22 <1021733560@qq.com>
2016-11-06 21:05:44 +0800

    添加个人中心的Fragment

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -218,7 +218,7 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
         mAdapter.addFragment(new ConversationListFragment(),getString(R.string.app_name));
         mAdapter.addFragment(new ContactListFragment(),getString(R.string.contacts));
         mAdapter.addFragment(new DiscoverFragment(),getString(R.string.discover));
-        mAdapter.addFragment(new SettingsFragment(),getString(R.string.me));
+        mAdapter.addFragment(new ProfileFragment(),getString(R.string.me));
         mAdapter.notifyDataSetChanged();
         layoutTabhost.setChecked(0);
         layoutTabhost.setOnCheckedChangeListener(this);
@@ -331,13 +331,13 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        layoutTabhost.setChecked(position);
-        layoutViewPage.setCurrentItem(position);
+
     }
 
     @Override
     public void onPageSelected(int position) {
-
+        layoutTabhost.setChecked(position);
+        layoutViewPage.setCurrentItem(position);
     }
 
     @Override
